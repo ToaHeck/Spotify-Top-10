3e9c94d884600bb73f344b0a1ded7969ae371061
To'a Heck <91290295+ToaHeck@users.noreply.github.com>
2025-04-02 16:10:07 -0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    clientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-    redirectUri: import.meta.env.VITE_SPOTIFY_PROD_REDIRECT_URI,
-    scopes: import.meta.env.VITE_SPOTIFY_SCOPES
-};
-
-export default config;
