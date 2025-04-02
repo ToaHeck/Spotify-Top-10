6507f2919323d0f38418dc01bbfe40d212391c1b
To'a Heck <91290295+ToaHeck@users.noreply.github.com>
2025-04-02 14:29:15 -0700

    Test

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 const config = {
-    clientId: `${import.meta.env.VITE_SPOTIFY_CLIENT_ID}` || 'd6e3508364bc452bb0175ba0dca1039d',
-    redirectUri: `${import.meta.env.VITE_SPOTIFY_PROD_REDIRECT_URI}` || 'https://stellular-maamoul-f64cd9.netlify.app/callback',
-    scopes: `${import.meta.env.VITE_SPOTIFY_SCOPES}` || 'user-read-currently-playing user-read-recently-played user-top-read user-read-private playlist-read-private user-read-playback-state'
+    clientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+    redirectUri: import.meta.env.VITE_SPOTIFY_PROD_REDIRECT_URI,
+    scopes: import.meta.env.VITE_SPOTIFY_SCOPES
 };
+
 export default config;
